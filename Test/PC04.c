338db2b03b64b374f21b87285ee76cfd84002fb6
ejanssen <ejanssen@student.42lausanne.ch>
2022-09-07 18:40:31 +0200

    normalize PC04

--- Test/PC04.c
+++ Test/PC04.c
@@ -11,11 +11,9 @@ int main()
 {
 
 	print_ex(0);
-	printf("%d \n",ft_strlen("Salut"));
-	printf("%d \n",ft_strlen(""));
-	printf("%d \n",ft_strlen(" Salut"));
-	printf("%d \n",ft_strlen(" Salut"));
-	printf("%lu \n",strlen(" Salut"));
+	printf("ft_strlen(\"Salut\")  = %d strlen(\"Salut\")  = %lu\n",ft_strlen("Salut"),strlen("Salut"));
+	printf("ft_strlen(\"\")       = %d strlen(\"\")       = %lu\n",ft_strlen(""),strlen(""));
+	printf("ft_strlen(\" Salut\") = %d strlen(\" Salut\") = %lu\n",ft_strlen(" Salut"),strlen(" Salut"));
 
 	print_ex(1);
 	ft_putstr("Salut");
@@ -25,7 +23,6 @@ int main()
 	print_ex(2);
 	ft_putnbr(10);
 	printf("\n");
-
 	ft_putnbr(0);
 	printf("\n");
 	ft_putnbr(30);
@@ -45,44 +42,46 @@ int main()
 	printf("\n");
 	print_ex(3);
 
-	printf("%d\n",ft_atoi("    --+---+1234dd"));
-	printf("%d\n",ft_atoi("    --+---+5346d%d45"));
-	printf("%d\n",ft_atoi("--+--+1234dd"));
-	printf("%d\n",ft_atoi("                   --+++-+1234dd"));
-	printf("%d\n",ft_atoi("    ++++---+12#34dd"));
-	printf("%d\n",ft_atoi("    --+---+1234dd"));
+	printf("ft_atoi(\"    --+---+1234dd\" )                 = %d\n",ft_atoi("    --+---+1234dd"));
+	printf("ft_atoi(\"    --+---+5346d/d45\" )              = %d\n",ft_atoi("    --+---+5346d/45"));
+	printf("ft_atoi(\"--+--+1234dd\" )                      = %d\n",ft_atoi("--+--+1234dd"));
+	printf("ft_atoi(\"                   --+++-+1234dd\" )  = %d\n",ft_atoi("                   --+++-+1234dd"));
+	printf("ft_atoi(\"    ++++---+12#34dd\" )               = %d\n",ft_atoi("    ++++---+12#34dd"));
+	printf("ft_atoi(\"    --+---+1234dd\" )                 = %d\n",ft_atoi("    --+---+1234dd"));
+	printf("ft_atoi(\"    --+-,-+1234dd\" )                 = %d\n",ft_atoi("    --+-,-+1234dd"));
 
 	print_ex(4);
-	printf("hex\n");
-	ft_putnbr_base(20, "0123456789ABCDEF");
-	printf("\n");
-	ft_putnbr_base(2, "0123456789ABCDEF");
-	printf("\n");
-	ft_putnbr_base(456, "0123456789ABCDEF");
-	printf("\n");
-	ft_putnbr_base(123456, "0123456789ABCDEF");
-	printf("\n");
-	printf("bin\n");
-	ft_putnbr_base(20, "01");
-	printf("\n");
-	ft_putnbr_base(-20, "01");
-	printf("\n");
-	ft_putnbr_base(2, "01");
-	printf("\n");
-	ft_putnbr_base(456, "01");
-	printf("\n");
-	ft_putnbr_base(123456, "01");
-	printf("\n");
-	ft_putnbr_base(-123456, "01");
+	for(int i =-100; i < 100; i += 3)
+	{
+		ft_putnbr(i);
+		ft_putstr("-> binary:               ");
+		ft_putnbr_base(i,"01");
+		printf("\n");
 
-	printf("\n");
-	printf("oc\n");
-	ft_putnbr_base(20, "poneyvif");
-	printf("\n");
-	ft_putnbr_base(2, "poneyvif");
-	printf("\n");
-	ft_putnbr_base(456, "poneyvif");
-	printf("\n");
-	ft_putnbr_base(123456, "poneyvif");
+		ft_putnbr(i);
+		ft_putstr("-> quartenary:           ");
+		ft_putnbr_base(i,"0123");
+		printf("\n");
+
+		ft_putnbr(i);
+		ft_putstr("-> octal                 ");
+		ft_putnbr_base(i,"poneyvif");
+		printf("\n");
+
+		ft_putnbr(i);
+		ft_putstr("-> hexadecimal           ");
+		ft_putnbr_base(i,"0123456789ABCDEF");
+		printf("\n");
+
+		ft_putnbr(i);
+		ft_putstr("-> duotrigesimal         ");
+		ft_putnbr_base(i,"0123456789ABCDEFGHIJKLMNOPQRSTUV");
+		printf("\n");
+
+		ft_putnbr(i);
+		ft_putstr("-> tetrasexagesimal      ");
+		ft_putnbr_base(i,"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/");
+		printf("\n");
+	}
 	return 0;
 }
